b947ca8e4e9628d9c62594012443340033dd2edd
CulmoneY <yculmone@rogers.com>
2024-09-09 18:04:47 -0400

    Added main method, whilst fixing doFizzBuss commands

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.net.SocketOption;
+import java.net.SocketTimeoutException;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(new String[]{});
+    }
+}
